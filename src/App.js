a8939f2db78eed55a9268a96271c1389fd23096c
hellomac87 <hellomac87@gmail.com>
2018-11-17 22:50:12 +0900

    react router 공부즁

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component, Provider } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React, { Component} from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from './components/common/Header';
 import Carousel from './components/main/Carousel';
 import Categories from './components/main/Categories';
@@ -8,23 +8,10 @@ import SeachBestStore from './components/main/SeachBestStore'
 import './App.scss';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      user : {
-        id : 1,
-        username: '이강산',
-        fullAddress: '서울시 노원구 상계동 666, 주공 10단지',
-        shortAddress: '상계동 666'
-      }
-    }
-  }
   render() {
-    const {user} = this.state;
     return (
       <Router>
         <div className="App">
-          <Header user={user} />
           <Route path="/" exact component={Main} />
           <Route path="/stores" component={Stores} />
         </div>
@@ -33,32 +20,56 @@ class App extends Component {
   }
 }
 
-const Main = () => {
-  return(
-    <div>
-      <Carousel />
-      <Categories />
-      <SeachBestStore />
-    </div>
-    
-  )
+class Main extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        id: 1,
+        username: '이강산',
+        fullAddress: '서울시 노원구 상계동 666, 주공 10단지',
+        shortAddress: '상계동 666'
+      }
+    }
+  }
+  componentDidMount(){
+    console.log('main component mount');
+  }
+  render(){
+    const { user } = this.state;
+    return (
+      <div>
+        <Header user={user} />
+        <Carousel />
+        <Categories />
+        <SeachBestStore />
+      </div>
+
+    )
+  }
 }
 
-const Stores = () => {
-  return(
-    <div>
-      상점페이지상점페이지상점페이지상점페이지
-      상점페이지상점페이지상점페이지상점페이지
-      상점페이지상점페이지상점페이지상점페이지
-      상점페이지상점페이지상점페이지상점페이지
-      상점페이지상점페이지상점페이지상점페이지
-      상점페이지상점페이지상점페이지상점페이지
-      상점페이지상점페이지상점페이지상점페이지
-      상점페이지상점페이지상점페이지상점페이지
-      상점페이지상점페이지상점페이지상점페이지
-      상점페이지상점페이지상점페이지상점페이지
-    </div>
-  )
+class Stores extends Component {
+  componentDidMount(){
+    console.log('main component mount');
+  }
+  render(){
+    return (
+      <div className="App">
+        상점페이지상점페이지상점페이지상점페이지
+        상점페이지상점페이지상점페이지상점페이지
+        상점페이지상점페이지상점페이지상점페이지
+        상점페이지상점페이지상점페이지상점페이지
+        상점페이지상점페이지상점페이지상점페이지
+        상점페이지상점페이지상점페이지상점페이지
+        상점페이지상점페이지상점페이지상점페이지
+        상점페이지상점페이지상점페이지상점페이지
+        상점페이지상점페이지상점페이지상점페이지
+        상점페이지상점페이지상점페이지상점페이지
+      </div>
+    );
+  }
+  
 }
 
 export default App;
