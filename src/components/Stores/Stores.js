1084d03d7d6c4150a465fd12437ac1a0416534ef
hellomac87 <hellomac87@gmail.com>
2018-11-18 18:45:05 +0900

    배밀 데모 작성해보는중

--- src/components/Stores/Stores.js
+++ src/components/Stores/Stores.js
@@ -7,29 +7,55 @@ class Stores extends Component {
         super(props);
         this.state = {
             lists: ['한식', '분식', '돈까스·회·일식', '치킨', '피자', '중국집', '족발·보쌈', '야식', '찜·탕', '도시락', '카페·디저트', '패스트푸드', '프랜차이즈', '맛집랭킹'],
-            currentCategory: null
+            currentCategory: null,
+            currentLists: null,
         }
     }
     componentDidMount() {
-        console.log('stores component mount');
+        // 현재 카테고리 설정하기
+        this.setState({
+            currentCategory: '한식',
+            currentLists: [
+                {
+                    id: 1,
+                    mainUrl: 'https://www.bloter.net/wp-content/uploads/2018/01/unnamed.png',
+                    title: '가게이름',
+                    description: '가게설명',
+                    likes: 100,
+                    reviews: 100
+                },
+                {
+                    id: 2,
+                    mainUrl: 'https://www.bloter.net/wp-content/uploads/2018/01/unnamed.png',
+                    title: '가게이름2',
+                    description: '가게설명2',
+                    likes: 100,
+                    reviews: 100
+                }
+            ]
+        });
+        // 현재 카테고리 설정을 바탕으로 현재 리스트 설정하기
     }
     render() {
-        const { lists, currentCategory } = this.state;
+        const { lists, currentCategory, currentLists } = this.state;
         return (
             <div className="stores">
                 <StoresHeader lists={lists} currentCategory={currentCategory}/>
-                <div>
-                    상점페이지상점페이지상점페이지상점페이지
-                    상점페이지상점페이지상점페이지상점페이지
-                    상점페이지상점페이지상점페이지상점페이지
-                    상점페이지상점페이지상점페이지상점페이지
-                    상점페이지상점페이지상점페이지상점페이지
-                    상점페이지상점페이지상점페이지상점페이지
-                    상점페이지상점페이지상점페이지상점페이지
-                    상점페이지상점페이지상점페이지상점페이지
-                    상점페이지상점페이지상점페이지상점페이지
-                    상점페이지상점페이지상점페이지상점페이지
-                </div>
+                <ul>
+                    {
+                        currentLists && currentLists.map((list,index) => {
+                            return (
+                                <li key={list.id}>
+                                    <img src={list.mainUrl} />
+                                    <div>{list.title}</div> 
+                                    <div>{list.description}</div> 
+                                    <div>{list.likes}</div> 
+                                    <div>{list.reviews}</div> 
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
             </div>
             
         );
