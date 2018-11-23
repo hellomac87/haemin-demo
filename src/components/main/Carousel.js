1193c2b1d1d359143c9e399c485587248a6cd754
hellomac87 <hellomac87@gmail.com>
2018-11-23 23:54:13 +0900

    react-responsive-carousel 적용해보기

--- src/components/main/Carousel.js
+++ src/components/main/Carousel.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import './Carousel.scss';
+import React, { Component } from 'react';
+// import './Carousel.scss';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
-const Carousel = () => {
-    return (
-        <div className="carousel">
-            Carousel
-        </div>
-    );
-};
+class MyCarousel extends Component {
+    render() {
+        return (
+            // Carousel설명서 :: http://react-responsive-carousel.js.org/storybook/?selectedKind=Carousel&selectedStory=PropTypes&full=0&down=1&left=1&panelRight=0&downPanel=kadirahq%2Fstorybook-addon-actions%2Factions-panel
+            <Carousel showThumbs={false} showIndicators={false} showArrows={false}>
+                <div>
+                    <img src="http://img.woowahan.com/www/common/baemin.jpg" alt="gf"/>
+                </div>
+                <div>
+                    <img src="http://img.woowahan.com/www/common/baemin.jpg" alt="gf"/>
+                </div>
+                <div>
+                    <img src="http://img.woowahan.com/www/common/baemin.jpg" alt="gf"/>
+                </div>
+            </Carousel>
+        );
+    }
+}
 
-export default Carousel;
+export default MyCarousel;
