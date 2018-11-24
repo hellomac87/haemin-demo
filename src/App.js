a7ed35ff02aa79f5ea1e371569c1308c3fba4298
hellomac87 <hellomac87@gmail.com>
2018-11-25 01:22:05 +0900

    Create :: Join 페이지 작성

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Main from './components/Main/Main';
 import Stores from './components/Stores/Stores';
 import Cart from './components/Cart';
 import Login from './components/Auth/Login';
-// import Join from './components/Auth/Join';
+import Join from './components/Auth/Join';
 import UserModal from './components/common/UserModal';
 import Footer from './components/common/Footer';
 
@@ -23,7 +23,8 @@ class App extends Component {
               {/* FIXME :: cart 컴포넌트는 url 변경을 통해 접근하는 것이 아니기 때문에 Route 방식으로 작성하지 않는다 */}
               <Route path="/cart" render={() => <Cart />} />
               <Route path="/login" component={Login} />
-              {/* <Route path="/Join" component={Join} /> */}
+              <Route path="/Join" component={Join} />
+              
               <UserModal />
               <Footer/>
             </div>
---
+++ src/components/Auth/Join.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+
+import './Join.scss';
+
+class Join extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    handleChange = (e) => {
+        console.log(e.target.value);
+    }
+    render() {
+        return (
+            <div className={classNames('join')}>
+                <div className={classNames('join-header')}>
+                    <h1>회원가입</h1>
+                    <button className={classNames('btn-complete','complete')}>완료</button>
+                </div>
+                <form className={classNames('join-body')}>
+                    <div className="join-body-el">
+                        <label htmlFor="nickname">닉네임</label>
+                        <input 
+                        onChange={(e) => this.handleChange(e)}
+                        // value={}
+                        type="text" name="nickname" placeholder="2 - 10자로 입력해주세요." />
+                        <span className="delete">d</span>
+                        <span className="complete">c</span>
+                    </div>
+                    <div className="join-body-el">
+                        <label htmlFor="email">이메일,아이디</label>
+                        <input 
+                        onChange={(e) => this.handleChange(e)}
+                        // value={}
+                        type="e-mail" name="email" placeholder="example@baemin.com"/>
+                        <span className="delete">d</span>
+                        <span className="complete">c</span>
+                    </div>
+                    <div className="join-body-el">
+                        <label htmlFor="password">비밀번호</label>
+                        <input 
+                        onChange={(e) => this.handleChange(e)}
+                        // value={}
+                        type="password" name="password" placeholder="8 - 20자로 입력해주세요." />
+                        <span className="delete">d</span>
+                        <span className="complete">c</span>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Join;
